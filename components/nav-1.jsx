8dfe18f2508b237d88f386f36fb9452b38652c8e
saman <sam.karimi727@gmail.com>
2025-03-06 00:15:23 +0330

    changing name

--- components/nav-1.jsx
+++ components/nav-1.jsx
@@ -9,7 +9,7 @@ const NavInfo = () => {
           <li><Link href="#">قوانین</Link></li>
           <li><Link href="#">تماس با ما</Link></li>
         </ul>
-        <h4>سفر به شیراز با <span class="nav-head-title">شیراز مبله</span></h4>
+        <h4>سفر به تمام نقاط ایران با <span class="nav-head-title">سفر لند</span></h4>
 
         <Link href="call to:09914318174">09914318174</Link>
       </div>
