eddb715feab0a09ce78a81f043b65ff2ed49865e
saman <sam.karimi727@gmail.com>
2025-03-07 00:51:41 +0330

    ss

--- constance/index.js
+++ constance/index.js
@@ -4,11 +4,54 @@
 export const header_Slidde_src = [
     {
         id : 1 ,
+        src : '/header-carousel-slide1.jpg'
+    },
+    {
+        id : 2 ,
+        src : '/header-carousel-slide2.jpg'
+    },
+    {
+        id : 3 ,
+        src : '/header-carousel-slide3.jpg'
+    },
+
+]
+export const destination_Slidde_src = [
+    {
+        id : 1 ,
         src : '../../public/header-carousel-slide1.jpg'
     },
     {
         id : 2 ,
         src : '../../public/header-carousel-slide2.jpg'
+    },
+    {
+        id : 3 ,
+        src : '../../public/header-carousel-slide3.jpg'
+    },
+    {
+        id : 3 ,
+        src : '../../public/header-carousel-slide3.jpg'
+    },
+    {
+        id : 3 ,
+        src : '../../public/header-carousel-slide3.jpg'
+    },
+    {
+        id : 3 ,
+        src : '../../public/header-carousel-slide3.jpg'
+    },
+    {
+        id : 3 ,
+        src : '../../public/header-carousel-slide3.jpg'
+    },
+    {
+        id : 3 ,
+        src : '../../public/header-carousel-slide3.jpg'
+    },
+    {
+        id : 3 ,
+        src : '../../public/header-carousel-slide3.jpg'
     },
     {
         id : 3 ,
