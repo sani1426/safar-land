1f48f9ba3a6a6d1559b0a5e7efbaa09003303179
saman <sam.karimi727@gmail.com>
2025-03-07 00:45:39 +0330

    style

--- components/Header/HeaderLider.jsx
+++ components/Header/HeaderLider.jsx
@@ -4,14 +4,22 @@ import React from 'react';
 import { Carousel } from 'antd';
 import { header_Slidde_src } from '@/constance';
 
+const contentStyle = {
+    height: '60vh',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    width : '100vw',
+    borderRadius : '10px'
+  };
 
 const HeaderCarousel = () => (
   <Carousel autoplay  draggable={true} autoplaySpeed={2000} infinite={true}>
 
     {
         header_Slidde_src.map(img => (
-            <div  key={img.id}>
-            <img  src={img.src} alt="" />
+            <div className={contentStyle}  key={img.id}>
+            <img className='w-full h-full rounded-md object-cover'  src={img.src} alt="" />
           </div>
         ))
     }
