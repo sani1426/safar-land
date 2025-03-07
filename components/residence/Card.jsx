87e933b8e87f38eccfc1bd43d35d254466a71b56
saman <sam.karimi727@gmail.com>
2025-03-07 04:03:58 +0330

    card

--- components/residence/Card.jsx
+++ components/residence/Card.jsx
@@ -1,15 +1,16 @@
 
 
-import React from 'react';
-import { Card } from 'antd';
-const { Meta } = Card;
-const CardItem = ({src , title}) => (
-  <Card
-    hoverable
-  
-    cover={<img alt={title} src={src} />}
-  >
-    <Meta title="Europe Street beat" description="www.instagram.com" />
-  </Card>
-);
+const CardItem = ({src , title}) =>{
+  return (
+    <div className="min-w-[300px] h-[250px] rounded-lg shadow-md hover:shadow-xl transition-all duration-500 mb-4">
+      <img className="w-full rounded-md h-[220px]" src={src} alt="" />
+      <h1 className="text-2xl text-center">{title}</h1>
+      <h3 className="px-3">سامان کریمی</h3>
+      <div className="flex justify-between p-3">
+        <p>{title}</p>
+        <p>{title}</p>
+      </div>
+    </div>
+  )
+}
 export default CardItem;
