e8010220c3cb61f7969828c49b09bec59e80f7c4
saman <sam.karimi727@gmail.com>
2025-03-07 03:53:09 +0330

    res

--- components/residence/ResContainer.jsx
+++ components/residence/ResContainer.jsx
@@ -2,12 +2,12 @@ import Residence from "./Residence";
 
 const ResContainer = () => {
     return ( 
-        <section className="w-full overflow-hidden bg-gray-200">
+        <div className="w-full overflow-hidden bg-gray-200">
             <div>
                 <h1>اقامتگاه های جدید</h1>
             </div>
             <Residence />
-        </section>
+        </div>
      );
 }
  
