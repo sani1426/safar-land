c34faeba20a4bb667c5a83f66423eb4449583c83
saman <sam.karimi727@gmail.com>
2025-03-10 00:06:27 +0330

    footer added

--- components/feature/feature.jsx
+++ components/feature/feature.jsx
@@ -2,25 +2,28 @@ import { featureCard } from '@/constance';
 import { Row } from 'antd';
 import FeatureCard from './featureCard';
 
-
 const Feature = () => {
-  return(
+  return (
     <div>
-    <Row gutter={16}>
-    {
-        featureCard.map(item =>(
-           <FeatureCard key={item.id} title={item.title} desc={item.desc} icon={item.icon} />
-        ))
-    }
-
-  </Row>
-  <div>
-    <h2 className='text-xl'>
-    کجارو با تهیه محتوا برای شهرهای مختلف ایران جهان به مرجعی کامل و معتبر در معرفی انواع جاهای دیدنی گردشگری، هتل‌ها و رستوران‌های ایران و جهان تبدیل شده است.
-    </h2>
-  </div>
-  </div>
-  )
+      <Row gutter={16}>
+        {featureCard.map((item) => (
+          <FeatureCard
+            key={item.id}
+            title={item.title}
+            desc={item.desc}
+            icon={item.icon}
+          />
+        ))}
+      </Row>
+      <div>
+        <h2 className='text-xl'>
+          کجارو با تهیه محتوا برای شهرهای مختلف ایران جهان به مرجعی کامل و معتبر
+          در معرفی انواع جاهای دیدنی گردشگری، هتل‌ها و رستوران‌های ایران و جهان
+          تبدیل شده است.
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,10 +1,75 @@
-
-
+import Link from 'next/link';
+import SocialIcons from './socialIcons';
 
 const Footer = () => {
-    return ( 
-        <></>
-     );
-}
- 
+  return (
+    <>
+      <footer className='bg-gray-200 p-12'>
+        <div className='grid grid-cols-1 text-center md:grid-cols-2'>
+          <div className='flex items-center justify-between'>
+            <div>
+              <h2 className='mb-8 text-2xl'>دسترسی سریع</h2>
+              <ul className='flex flex-col gap-4'>
+                <li>
+                  <Link>خانه</Link>
+                </li>
+                <li>
+                  <Link>درباره ما</Link>
+                </li>
+                <li>
+                  <Link>تماس با ما</Link>
+                </li>
+                <li>
+                  <Link>اقامتگاه ها</Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className='mb-8 text-2xl'>حامیان سفرلند</h2>
+              <ul className='flex flex-col gap-4'>
+                <li>
+                  <Link>آقای تشریفات</Link>
+                </li>
+                <li>
+                  <Link>سامان کریمی</Link>
+                </li>
+                <li>
+                  <Link>سجاد کریمی</Link>
+                </li>
+                <li>
+                  <Link>مهدی کریمی</Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className='mb-8 text-2xl'>حامیان سفرلند</h2>
+              <ul className='flex flex-col gap-4'>
+                <li>
+                  <Link>آقای تشریفات</Link>
+                </li>
+                <li>
+                  <Link>سامان کریمی</Link>
+                </li>
+                <li>
+                  <Link>سجاد کریمی</Link>
+                </li>
+                <li>
+                  <Link>مهدی کریمی</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='mx-auto my-auto'>
+            <SocialIcons />
+          </div>
+        </div>
+      </footer>
+      <div className='flex h-12 w-full items-center justify-center rounded-md bg-orange-400'>
+        <h1>کلیه حقوق این وبسایت متعلق به سامان کریمی است</h1>
+      </div>
+    </>
+  );
+};
+
 export default Footer;
